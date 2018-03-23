86c6f232a44a4b581f50fc5bf758aaa7ec12aee8
EC2 Default User <ec2-user@ip-172-31-25-186.us-east-2.compute.internal>
2018-03-23 00:02:30 +0000

    Add BigHoss.py

---
+++ BigHoss.py
@@ -0,0 +1,44 @@
+
+import discord
+import asyncio
+
+bigHoss = discord.Client()
+
+@bigHoss.event
+async def on_ready():
+    print('Logged in as')
+    print(bigHoss.user.name)
+    print(bigHoss.user.id)
+    print(bigHoss.user.mention)
+    print('------')
+    await bigHoss.change_presence(game=discord.Game(name="NAM '67"))
+    await bigHoss.send_message(bigHoss.get_channel("247243512519720961"), 
+        "BIG HOSS IS ONLINE, WELCOME TO THE NAM >:o")
+
+
+
+@bigHoss.event
+async def on_message(message):
+
+    if message.content.startswith('!BigHoss'):
+        await bigHoss.send_message(message.channel, 'HEY')
+    if message.content.startswith("!NAM"):
+        await bigHoss.send_message(message.channel, "SHIT WERE IN THE NAM!!")
+    if message.content.startswith("!BirdDog"):
+        await bigHoss.send_message(message.channel, "WE WE GOT A NEW GUY TODAY, call em bird dog.")
+
+    if message.content.startswith("!Charlie"):
+        await bigHoss.send_message(message.channel, "Get down {0}!, It's charlie!!".format(message.author.mention))
+    if message.content.startswith("!Heck"):
+        await bigHoss.send_message(message.channel, "Heck,{0}, I don't know if we're ganna make it".format(message.author.mention))
+
+
+    if message.content.startswith("!CallSeth"):
+        server = bigHoss.get_server("247243512519720961")
+        seth = server.get_member("192844733746380801")
+        await bigHoss.send_message(message.channel, "{0}, you're a lil bich".format(seth.mention))
+
+bigHoss.run('Mzg2Mjk4NjI4NjU1OTM5NTg3.DZXArA._dJBK8n8aij6c2h03XQfvkUs5AQ')
+
+
+
